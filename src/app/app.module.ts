def15ce568072d64dbebb716a3be3e64161f1f2c
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-10-30 15:44:36 -0400

    rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { ProductosComponent } from './paginas/productos/productos.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ProductosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
